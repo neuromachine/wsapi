f5b23c4b060575442850a8a82d3fea405522bd55
Nero <neuromacine@gmail.com>
2025-07-13 11:38:07 +0700

    filament: propertyValues fields

--- app/Models/BlockItemPropertyValue.php
+++ app/Models/BlockItemPropertyValue.php
@@ -10,4 +10,12 @@ class BlockItemPropertyValue extends Model
     {
         return $this->belongsTo(BlockItemProperty::class, 'property_id');
     }
+
+    // Отношения (уже должны быть)
+    public function item()
+    {
+        return $this->belongsTo(BlockItem::class, 'item_id');
+    }
+
+
 }
