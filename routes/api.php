2b2f71d12d1108982e61fec32b95b751fe8f8b82
Nero <neuromacine@gmail.com>
2026-03-01 19:23:43 +0700

    refactor: clear legacy router

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,9 @@
 <?php
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\Api\DictionaryController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ItemController;
 use \App\Http\Controllers\Api\BlockCategoryController;
 use \App\Http\Controllers\Api\BlockController;
 use \App\Http\Controllers\Api\BlockItemController;
 use App\Http\Controllers\Api\FormController;
-
-Route::get('/dictionaries/{id}/categories/tree', [CategoryController::class, 'fullTree']);
-Route::get('/categories/{slug}/items', [CategoryController::class, 'items']);
-Route::get('/tree/{slug?}', [CategoryController::class, 'index']);
-Route::get('/dictionaries/{key}/items', [DictionaryController::class, 'items']);
-
-Route::get('/item/{slug}', [ItemController::class, 'getItemBySlug']);
-Route::get('/cat/{slug}', [CategoryController::class, 'show']);
 
 Route::prefix('blocks/blocks')
     ->controller(BlockController::class)
