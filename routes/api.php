c93e7d2cc814e530e6a3987af63f5567a9ab1754
Nero <neuromacine@gmail.com>
2026-03-01 19:16:06 +0700

    fix:clear legacy router

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,7 @@ use App\Http\Controllers\ItemController;
 use \App\Http\Controllers\Api\BlockCategoryController;
 use \App\Http\Controllers\Api\BlockController;
 use \App\Http\Controllers\Api\BlockItemController;
-
-
 use App\Http\Controllers\Api\FormController;
-
-use Illuminate\Support\Facades\Mail;
-use Illuminate\Http\Request;
-//sleep(3);
-
-Route::get('/test', function () {
-    $users = DB::table('users')->get();
-    return response()->json($users);
-});
 
 Route::get('/dictionaries/{id}/categories/tree', [CategoryController::class, 'fullTree']);
 Route::get('/categories/{slug}/items', [CategoryController::class, 'items']);
@@ -27,15 +16,6 @@ Route::get('/dictionaries/{key}/items', [DictionaryController::class, 'items']);
 
 Route::get('/item/{slug}', [ItemController::class, 'getItemBySlug']);
 Route::get('/cat/{slug}', [CategoryController::class, 'show']);
-
-//Route::get('/group/offers/{slug}', [CategoryController::class, 'offers']);
-
-
-#Route::get('/group/offers/{slug}', [BlockCategoryController::class, 'offers']);
-
-#Route::get('blocks/structure/{slug?}', [BlockCategoryController::class, 'structure']);
-
-#Route::get('block-categories/{slug}/structure', [BlockCategoryController::class, 'structure']);
 
 Route::prefix('blocks/blocks')
     ->controller(BlockController::class)
@@ -51,17 +31,6 @@ Route::prefix('blocks/categories')
         Route::get('{slug}', 'index')->name('index');
         Route::get('structure/{slug?}', 'structure')->name('structure');
         Route::get('offers/{slug}', 'offers')->name('offers');
-/*
-        // Корневые действия без идентификатора
-        Route::get('structure', 'rootStructure')->name('structure.root');
-        Route::get('create', 'create')->name('create');
-
-        // Если понадобится ID вместо slug
-        Route::get('edit/{id}', 'edit')->name('edit');
-
-        // Универсальный маршрут с GET-параметрами
-        Route::get('search', 'search')->name('search');
-        */
     });
 
 Route::prefix('blocks/items')
