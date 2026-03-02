0b2168cbbaa2c9df32bb6154729fdb76bdf5debf
Nero <neuromacine@gmail.com>
2026-03-02 20:27:28 +0700

    MOD: locale - items

--- app/Repositories/BlockItemRepository.php
+++ app/Repositories/BlockItemRepository.php
@@ -7,15 +7,19 @@ use Illuminate\Database\Eloquent\Collection;
 class BlockItemRepository
 {
 
-    public function getItem(string $key)
+    public function getItem(string $locale, string $key)
     {
 
-        return BlockItem::with(
-                [
-                    'propertyValues.property',
-                ]
-            )
+        return BlockItem::with([
+            'propertyValues' => function ($q) use ($locale) {
+                $q->where('locale', $locale);
+            },
+            'propertyValues.property',
+        ])
             ->where('key', $key)
+            ->whereHas('propertyValues', function ($q) use ($locale) {
+                $q->where('locale', $locale);
+            })
             ->firstOrFail();
     }
 
